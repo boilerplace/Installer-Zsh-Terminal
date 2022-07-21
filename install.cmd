959d0c33d9cca89caaf00c9fe7f4105b2534f5c9
Luis Alfredo G. Caldas Neto <luisnt@gmail.com>
2022-07-21 14:56:33 -0300

    Reported errors have been resolved

--- install.cmd
+++ install.cmd
@@ -1,5 +1,8 @@
 @echo off
+@chcp 65001
+
 @cls
+@SET "SEP=)" 
 @SET "PER=%%%%" 
 @SET "SITE=http://mirrors.kernel.org/sourceware/cygwin"
 @SET "APP_PATH=%cd%" 
@@ -9,38 +12,79 @@
 @SET "BIN=%APP%\bin" 
 @SET "SHOTCUT_NAME=%USERPROFILE%\desktop\Zsh.url"
 
-@call %SRC%\dieZsh.cmd && @cls
+if exist "%windir%\fonts\MesloLGS NF Regular.ttf" goto continue
 
-@md %APP% > nul && @cls
-@md %BIN% > nul && @cls
-@md %TMP% > nul && @cls
-
+:info
 @cls
-@echo Making %APP%\Cygwin-integrated.bat
+echo.                                                       
+ECHO ┌─── ℹ INFO ─────────────────────────────────────────┐
+ECHO │                                                    │
+ECHO │  BEFORE CONTINUE                                   │
+ECHO │  YOU NEED INSTALL THE DEPENDENCIES                 │
+ECHO │  FONTS MESLOLGS NF ON OPENNED FOLDER               │
+ECHO │                                                    │
+ECHO │                                       BY LUIS NT   │
+ECHO └────────────────────────────────────────────────────┘
+ECHO.                                                       
+goto checkfonts
+
+:warning
+@cls
+ECHO.
+ECHO ┌─── ⚠ WARNING ──────────────────────────────────────┐
+ECHO │                                                    │
+ECHO │  ERROR: %DATE% %TIME:~0,8% - FONTS NOT FOUND      │
+ECHO │                                                    │
+ECHO ├────────────────────────────────────────────────────┤
+ECHO │                                                    │
+ECHO │  BEFORE CONTINUE                                   │
+ECHO │  YOU NEED INSTALL THE DEPENDENCIES                 │
+ECHO │  FONTS MESLOLGS NF ON OPENNED FOLDER               │
+ECHO │                                                    │
+ECHO │                                       BY LUIS NT   │
+ECHO └────────────────────────────────────────────────────┘
+ECHO.
+
+:checkfonts
+@start %SRC%\fonts\
+pause
+
+if not exist "%windir%\fonts\MesloLGS NF Regular.ttf" goto warning
+
+:continue
+
+@call %SRC%\dieZsh.cmd
+
+if not exist "%APP%" @md %APP% > nul
+if not exist "%BIN%" @md %BIN% > nul
+if not exist "%TMP%" @md %TMP% > nul
+
+@echo Copping files from %SRC% to %APP%
+@copy %SRC%\zsh.ico %APP%\zsh.ico >nul
+@copy %SRC%\dieZsh.bat %APP%\dieZsh.bat >nul
+@copy %SRC%\setup-x86_64.exe %BIN%\setup-x86_64.exe >nul
+@echo Copied fully - OK
+
+echo.
+@echo Extracting presseting of the "%SRC%\home\user.parts.rar" to "%APP%\home\%USERNAME%\"
+@%SRC%\unrar x -va -inul -y "%SRC%\home\user.part01.rar" "%APP%\home\%USERNAME%\"
+@echo Extracted fully - OK
+
+echo.
+@echo Download of the Zsh dependencies to %APP%
+@%BIN%\setup-x86_64.exe -q --no-admin -n -N -l %TMP% -R %APP% -s %SITE%% -P zsh,bash,wget,lynx,git,chere >nul
+@echo Downloaded fully - OK
+
+echo.
+@echo Creating Batch to open %APP%\Cygwin-integrated.bat
 @echo @echo off>%APP%\Cygwin-integrated.bat
 @echo set path = %BIN%;%PER:~1,1%PATH%PER:~1,1%>>%APP%\Cygwin-integrated.bat
 @echo set CHERE_INVOKING=1>>%APP%\Cygwin-integrated.bat
 @echo %BIN%\zsh.exe --login -i>>%APP%\Cygwin-integrated.bat
-@echo Completed %APP%\Cygwin-integrated.bat
-@echo Copping icon Terminal.ico to %APP%
-@copy %SRC%\zsh.ico %APP%\zsh.ico >nul
-@echo Copping utility of process dieZsh to %APP%
-@copy %SRC%\dieZsh.bat %APP%\dieZsh.bat >nul
-@echo Copping alias apt-get debian for apt to %BIN%
-@copy %SRC%\apt %BIN%\apt >nul
-@echo Copping alias cls for clear to %APP%
-@copy %SRC%\cls %BIN%\cls >nul
-@echo Copping installer Cygwin64 to %APP%
-@copy %SRC%\setup-x86_64.exe %BIN%\setup-x86_64.exe >nul
-@echo Download the dependencies zsh,bash,wget,git,chere to %APP%
-@%BIN%\setup-x86_64.exe -q --no-admin -n -N -l %TMP% -R %APP% -s %SITE%% -P zsh,bash,wget,lynx,git,chere >nul
-@echo Extracting the home/root to %APP%
-@%SRC%\unrar x -v %SRC%\home\root.part01.rar %APP%\home\
-@echo Call icon Cygwin64 Terminal to open Zsh shell
+@echo Created fully - OK
+
 @echo.
-
-@echo Creating Batch to open Zsh
-
+@echo Creating Batch to open Zsh %APP%\Cygwin.bat
 @echo @echo off > %APP%\Cygwin.bat
 @echo set path = %BIN%;%PER:~1,1%PATH%PER:~1,1% >> %APP%\Cygwin.bat
 @echo @%APP%\dieZsh.bat >> %APP%\Cygwin.bat
@@ -48,12 +92,28 @@
 @echo @setlocal enableextensions >> %APP%\Cygwin.bat
 @echo @set TERM= >> %APP%\Cygwin.bat
 @echo @%BIN%\mintty.exe -i /zsh.ico %BIN%\zsh --login >> %APP%\Cygwin.bat
+@echo Created fully - OK
 
+@echo.
 @echo Creating Icon %SHOTCUT_NAME% 
-
 @echo [InternetShortcut] > %SHOTCUT_NAME%
 @echo URL="%APP%\Cygwin.bat" >> %SHOTCUT_NAME%
 @echo IconFile=%APP%\zsh.ico >> %SHOTCUT_NAME%
 @echo IconIndex=0 >> %SHOTCUT_NAME%
+@echo Creation of the icon fully - OK
 
-@%SHOTCUT_NAME%
+@echo.
+@echo Open terminal from %SHOTCUT_NAME% 
+@start %SHOTCUT_NAME%
+@echo Openned fully - OK
+
+@echo.
+@echo ┌─── ℹ INFO ─────────────────────────┐
+@echo │                                    │
+@echo │  INSTALLATION COMPLETE.            │
+@echo │  THANK YOU FOR USING OUR PRODUCT.  │
+@echo │  YOU CAN NOW CLOSE THIS WINDOW.    │
+@echo │                                    │
+@echo │                        BY LUIS NT  │
+@echo └────────────────────────────────────┘
+@pause|more
--- src/dieZsh.cmd
+++ src/dieZsh.cmd
@@ -1,6 +1,6 @@
 @echo off
-@taskkill /IM gitstatusd-cygwin_nt-10.0-x86_64 /F>nul
-@taskkill /IM setup-x86_64.exe /F>nul
-@taskkill /IM bash.exe /F>nul
-@taskkill /IM zsh.exe /F>nul
+@tasklist | find /i "gitstatusd-cygwin_nt-10.0-x86_64" && @taskkill /im gitstatusd-cygwin_nt-10.0-x86_64 /F>nul
+@tasklist | find /i "setup-x86_64.exe" && @taskkill /im setup-x86_64.exe /F>nul
+@tasklist | find /i "bash.exe" && @taskkill /im bash.exe /F>nul
+@tasklist | find /i "zsh.exe" && @taskkill /im zsh.exe /F>nul
 @cls
